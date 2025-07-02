4882108ce725821a38314e01570e06445cea4223
Ultimate_Kevin <2784232369@qq.com>
2025-07-02 15:51:31 +0800

    Delete frontend directory

--- frontend/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-function App() {
-  return <h1>时光之轴：文明坐标</h1>;
-}
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
